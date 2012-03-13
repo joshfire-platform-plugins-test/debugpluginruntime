965707ab836a710600f96250fe5bef071c95fb9c
Sylvain Zimmer <sylvain@sylvainzimmer.com>
2012-03-13 14:59:52 +0100

    debug plugin

---
+++ build.js
@@ -0,0 +1,37 @@
+module.exports = {
+  "startfile":function(runtime, params, callback) {
+
+    params["content"] = runtime.headPrepend(params["content"],"PREHEAD $1 Iñtërnâtiônàlizætiøn ");
+    params["content"] = runtime.headAppend(params["content"],"POSTHEAD $1 Iñtërnâtiônàlizætiøn ");
+    params["content"] = runtime.bodyAppend(params["content"],"POSTBODY $1 Iñtërnâtiônàlizætiøn ");
+    params["content"] = runtime.bodyPrepend(params["content"],"PREBODY $1 Iñtërnâtiônàlizætiøn ");
+    
+    // TODO params.deploy.options
+    var paramsdump = [
+      params.options.opt1withdefault,
+      params.options.opt2withoutdefault,
+      params.deploy.type,
+      params.deploy.env,
+      params.deploy.isPhonegap
+    ];
+
+    callback(null, JSON.stringify(paramsdump)+params["content"]);
+
+  },
+  "bootstrap":function(runtime, params, callback) {
+    
+    // TODO params.deploy.options
+    var paramsdump = [
+      params.options.opt1withdefault,
+      params.options.opt2withoutdefault,
+      params.deploy.type,
+      params.deploy.env,
+      params.deploy.isPhonegap
+    ];
+
+    callback(null, params["content"]+"var POSTBOOTSTRAP="+JSON.stringify(paramsdump)+";");
+  },
+  "xcodeproj":function(runtime, params, callback) {
+    callback(null);
+  }
+};
