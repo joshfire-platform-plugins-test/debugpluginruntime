0381fcabb0f93248567941c826a7328e9da368ec
François Daoust <francois@joshfire.com>
2012-07-11 11:36:14 +0200

    Updated hooks handler definition to return a factory function.

--- list.js
+++ list.js
@@ -1,10 +1,34 @@
 /**
- * @fileoverview List hook definition
+ * @fileoverview List hook handler definition.
+ *
+ * See item.js for details.
  */
 define(['./dep'], function (dep) {
-  return {
-    generate: function (options, callback) {
-      return callback(null, '<p>List hook of debug add-on</p>');
-    }
+  /*
+   * The module returns a factory method that creates the add-on
+   * initialized with the given options
+   *
+   * @function
+   * @param {Object} config Add-on configuration options, generated from
+   *  the JSON Form object that the add-on may define in its manifest file
+   */
+  return function (config) {
+
+    return {
+      /**
+       * Generates the add-on's HTML content.
+       *
+       * Generation may be asynchronous. The callback function receives the
+       * error and the generated HTML content.
+       *
+       * @function
+       * @param {Object} options Generation options. TBD.
+       * @param {function} cb Callback function
+       */
+      generate: function (options, callback) {
+        console.log('addon', 'debugpluginruntime', 'list', 'generate', options);
+        return callback(null, '<p>List hook of debug add-on</p>');
+      }
+    };
   };
 });
